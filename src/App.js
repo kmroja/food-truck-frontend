2b624e1894f59b0ed3b15bef446d83383ad2feb2
kmroja <rojamahendiran825@gmail.com>
2023-03-26 19:10:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+import {Toaster} from 'react-hot-toast';
+
+import Main from "./Home";
+import Login from "./Login"
+
+export default function app() {
+    return (
+      <div className="home">
+        <Toaster
+     position="top-right"
+     toastOptions={{
+       style: {
+         fontSize: '1.8rem',
+       },
+     }}
+    />
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Main />} />
+        </Routes>
+      </div>
+    );
+  }
+
+
+
